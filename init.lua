561aef0f69100285728906a42cc8b72b1f460866
YellowButShort-kys <59924622+YellowButShort-kys@users.noreply.github.com>
2020-10-16 20:59:03 +0300

    First upload

---
+++ init.lua
@@ -0,0 +1,219 @@
+local yellows_rl = {}
+local shapes = require 'Yellows_raylight.HC.shapes'
+local blocking_objects = {}
+local light_sources = {}
+local circle_light
+local cone_light
+
+local function ccw (a,b,c) 
+    --if a looks at b, is c on the left or on the right? (points)
+    local num = ((b[1] - a[1])*(c[2]-a[2]))-((b[2]-a[2])*(c[1]-a[1]))
+    return num
+end
+local function lineLineCollision(lineA,lineB)
+    --will check whether lineA crosses lineB. Returns point of collision
+    local A = lineA[1] --A,B,C and D are the points that make up the lines
+    local B = lineA[2]
+    local C = lineB[1]
+    local D = lineB[2]
+    if ((ccw(A,B,C) * ccw (A,B,D)) < 0) and ((ccw(C,D,A) * ccw(C,D,B)) < 0) then
+        return true
+    else
+        return false
+    end
+end
+  
+function yellows_rl.Init()
+    circle_light = require "Yellows_raylight.lights.light_round"
+    cone_light = require "Yellows_raylight.lights.light_cone"
+end
+
+function yellows_rl.CreateCircleLight(x, y, radius)
+    assert(type(x)=="number", "X should be a number")
+    assert(type(y)=="number", "Y should be a number")
+    assert(type(radius)=="number", "Radius should be a number")
+
+    local source = {}
+    setmetatable(source, {__index = circle_light})
+    source.x = x
+    source.y = y
+    source.radius = radius
+    source.col = shapes.newCircleShape(x, y, radius)
+    table.insert(light_sources, source)
+
+    yellows_rl.UpdateIntersection()
+
+    return source
+end
+
+function yellows_rl.CreateConeLight(x, y, length, base, angle)
+    assert(type(x)=="number", "X should be a number")
+    assert(type(y)=="number", "Y should be a number")
+    assert(type(length)=="number", "Length should be a number")
+    assert(type(base)=="number", "Length should be a number")
+    assert(type(angle or 0)=="number", "Angle should be a number")
+
+    local source = {}
+    setmetatable(source, {__index = cone_light})
+    source.x = x
+    source.y = y
+    source.length = length
+    source.base = base
+    source.angle = angle or 0 * math.pi/180
+    source.col = shapes.newPolygonShape(x, y, x - base/2, y - length, x + base/2, y - length)
+    source.col:setRotation(angle or 0 * math.pi/180, x, y)
+    table.insert(light_sources, source)
+
+    yellows_rl.UpdateIntersection()
+
+    return source
+end
+
+function yellows_rl.CreateRectangonalBlocker(x, y, w, h)
+    local blocker = shapes.newPolygonShape(x, y, x+w, y, x+w, y+h, x, y+h)
+    blocker.polygons = {{x, y}, {x+w, y}, {x+w, y+h}, {x, y+h}}
+    blocker.lines = {
+        {{x,y}, {x+w, y}},
+        {{x+w, y}, {x+w, y+h}},
+        {{x+w, y+h}, {x, y+h}},
+        {{x, y+h}, {x, y}}
+    }
+    table.insert(blocking_objects, blocker)
+    return blocker
+end
+
+function yellows_rl.UpdateIntersection()
+    for _, source in ipairs(light_sources) do
+        local returned = {}
+        local x, y, radius = source:GetX(), source:GetY()
+        if source.GetRadius then
+            radius = source:GetRadius()
+        elseif source.GetLength then
+            radius = source:GetLength()
+        end
+        
+        for _, blocker in ipairs(blocking_objects) do
+            --if source.col:collidesWith(blocker) then
+                local prefinal = {}
+                local PointOrEnd = true
+                local nolinecurrent = {}
+                for _, polygons in ipairs(blocker.polygons) do
+                    local x1, y1 = unpack(polygons)
+                    local switch = true
+                    local current = {}
+
+                    if x == x1 then
+                        local nx1, nx2 = x1 - 1, x1 + 1
+                        local len = math.sqrt(math.pow(x - nx1, 2) + math.pow(y - y1, 2))
+                        local cx = nx1 + (nx1 - x) / len * 5
+                        local cy = y1 + (y1 - y) / len * 5
+                        if blocker:contains(cx, cy) then
+                            table.insert(nolinecurrent, x1)
+                            table.insert(nolinecurrent, y1)
+                            switch = false
+                        end
+
+                        len = math.sqrt(math.pow(x - nx2, 2) + math.pow(y - y1, 2))
+                        cx = nx2 + (nx2 - x) / len * 5
+                        cy = y1 + (y1 - y) / len * 5
+                        if blocker:contains(cx, cy) then
+                            table.insert(nolinecurrent, x1)
+                            table.insert(nolinecurrent, y1)
+                            switch = false
+                        end
+                    elseif y == y1 then
+                        local ny1, ny2 = y1 - 1, y1 + 1
+                        local len = math.sqrt(math.pow(x - x1, 2) + math.pow(y - ny1, 2))
+                        local cx = x1 + (x1 - x) / len * 5
+                        local cy = ny1 + (ny1 - y) / len * 5
+                        if blocker:contains(cx, cy) then
+                            table.insert(nolinecurrent, x1)
+                            table.insert(nolinecurrent, y1)
+                            switch = false
+                        end
+
+                        len = math.sqrt(math.pow(x - x1, 2) + math.pow(y - ny2, 2))
+                        cx = x1 + (x1 - x) / len * 5
+                        cy = ny2 + (ny2 - y) / len * 5
+                        if blocker:contains(cx, cy) then
+                            table.insert(nolinecurrent, x1)
+                            table.insert(nolinecurrent, y1)
+                            switch = false
+                        end
+                    else
+                        local len = math.sqrt(math.pow(x - x1, 2) + math.pow(y - y1, 2))
+                        local cx = x1 + (x1 - x) / len * 5
+                        local cy = y1 + (y1 - y) / len * 5
+                        if blocker:contains(cx, cy) then
+                            table.insert(nolinecurrent, x1)
+                            table.insert(nolinecurrent, y1)
+                            switch = false
+                        end
+                    end
+
+                    if switch then 
+                        for _, line in ipairs(blocker.lines) do
+                            if lineLineCollision({{x,y}, polygons}, line) then
+                                switch = false
+                            end
+                        end
+                    end
+
+                    if switch then
+                        local angle = -math.atan2(y1 - y, x1 - x) + math.pi/2
+                        local endx, endy = radius * math.sin(angle) + x, radius * math.cos(angle) + y
+                        len = math.sqrt(math.pow(x - endx, 2) + math.pow(y - endy, 2))
+                        endx = endx + (endx - x) / len * len * len * len
+                        endy = endy + (endy - y) / len * len * len * len
+
+                        current[1] = x1
+                        current[2] = y1
+                        current[3] = endx
+                        current[4] = endy
+
+                        table.insert(prefinal, current)
+                    end
+                end
+
+                local final = {}
+                table.insert(final, nolinecurrent[1])
+                table.insert(final, nolinecurrent[2])
+                for _, var in ipairs(prefinal) do
+                    if PointOrEnd then --looks like shit but who cares
+                        table.insert(final, var[1])
+                        table.insert(final, var[2])
+                        table.insert(final, var[3])
+                        table.insert(final, var[4])
+                        PointOrEnd = false
+                    else 
+                        table.insert(final, var[3])
+                        table.insert(final, var[4])
+                        table.insert(final, var[1])
+                        table.insert(final, var[2])
+                        PointOrEnd = true
+                    end
+                end
+                table.insert(returned, final)
+            --end
+        end
+
+        for _, var in ipairs(returned) do
+            for i = 1, #var-2, 2 do  --in case if points are duplicated
+                if var[i] == var[i+2] and var[i+1] == var[i+3] then
+                    table.remove(var, i)
+                    table.remove(var, i)
+                end
+            end
+        end
+
+        source:CalcDrawing(returned)
+    end
+end
+
+function yellows_rl.Draw()
+    for _, source in ipairs(light_sources) do
+        source:Draw()
+    end
+end
+
+return yellows_rl
---
+++ lights/light_round.lua
@@ -0,0 +1,71 @@
+local light_source = {texture = love.graphics.newImage("Yellows_raylight/textures/round.png")}
+local rl = require "Yellows_raylight"
+local shapes = require 'Yellows_raylight.HC.shapes'
+
+function light_source:SetX(x)
+   self.x = x
+   self.col:moveTo(x, self.y)
+   rl.UpdateIntersection()
+end
+function light_source:SetY(y)
+    self.y = y
+    self.col:moveTo(self.x, y)
+    rl.UpdateIntersection()
+end
+function light_source:SetPos(x, y)
+    self.x = x
+    self.y = y
+    self.col:moveTo(x, y)
+    rl.UpdateIntersection()
+end
+
+function light_source:GetX()
+    return self.x
+end
+function light_source:GetY()
+    return self.y
+end
+function light_source:GetPos()
+    return self.x, self.y
+end
+
+function light_source:SetRadius(radius)
+    self.radius = radius
+    rl.UpdateIntersection()
+end
+function light_source:GetRadius()
+    return self.radius
+end
+
+function light_source:SetColor(r, g, b, a)
+    if type(r) == "table" then
+        self.color = r
+    else
+        self.color = {r, g, b, a}
+    end
+end
+function light_source:GetColor()
+    return self.color
+end
+
+function light_source:CalcDrawing(polygons)
+    self.polygons = polygons
+    self.StencilFunc = function()
+        for _, var in ipairs(self.polygons) do
+            love.graphics.polygon("fill", var)
+        end
+    end
+end
+
+function light_source:Draw()
+    love.graphics.stencil(self.StencilFunc, "replace", 1)
+    love.graphics.setStencilTest("less", 1)
+
+    love.graphics.setColor(self.color or {1, 1, 1})
+    love.graphics.draw(self.texture, self.x-self.radius, self.y-self.radius, self.angle, self.radius*2/self.texture:getWidth())
+    love.graphics.setColor(1, 1, 1, 1)
+
+    love.graphics.setStencilTest()
+end
+
+return light_source
